4ee37ca7eadd5dc733f260c395f8de2d019a53d3
WGrot <117313232+WGrot@users.noreply.github.com>
2024-12-15 11:22:33 +0100

    Added simple enemy waves logic

--- Assets/Code/Scripts/Enemies/Spawner.cs
+++ Assets/Code/Scripts/Enemies/Spawner.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -8,14 +10,52 @@ public class Spawner : NetworkBehaviour
 	[SerializeField] private GameObject enemyPrefab;
 	[SerializeField] private EnemyPath defaultPath;
 
+	[SerializeField] private Wave[] waves;
+	private int activeWaveId = -1;
 
-	[ServerRpc(RequireOwnership = false)]
-	public void SpawnEnemyServerRpc()
+
+	public void Start()
 	{
-		GameObject spawnedEnemy = Instantiate(enemyPrefab);
-		spawnedEnemy.GetComponent<EnemyNavigation>().SetPath(defaultPath);
-		spawnedEnemy.GetComponent<NetworkObject>().Spawn(true);
+		foreach (Wave wave in waves)
+		{
+			wave.SetDeafaultServerPath(defaultPath);
+		}
 	}
+
+	[ServerRpc]
+	public void StartNextWaveServerRpc()
+	{
+		activeWaveId++;
+		waves[activeWaveId].StartNextWaveServerRpc();
+		/*
+		foreach(WaveEntry entry in waves[activeWaveId].WaveEntries)
+		{
+			for (int i =0; i< entry.EnemyAmount; i++)
+			{
+				GameObject spawnedEnemy = Instantiate(
+					entry.EnemyPrefab, 
+					waves[activeWaveId].gameObject.transform,
+					false);
+
+				if (entry.UseCustomPath)
+				{
+					spawnedEnemy.GetComponent<EnemyNavigation>().SetPath(entry.EnemyPath);
+				}
+				else
+				{
+					spawnedEnemy.GetComponent<EnemyNavigation>().SetPath(defaultPath);
+				}
+				spawnedEnemy.GetComponent<NetworkObject>().Spawn(true);
+
+			}
+
+		}
+		*/
+
+
+	}
+
+
 
 	private void OnDrawGizmosSelected()
 	{
