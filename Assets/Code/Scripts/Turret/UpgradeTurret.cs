c4416fc49e33b9f2582ea66700ea554da595d78e
Marcin Komza <01178858@pw.edu.pl>
2025-01-02 19:21:01 +0100

    Stan początkowy. Wyświetlana wiadomość debug po interakcji z wieżyczką.

---
+++ Assets/Code/Scripts/Turret/UpgradeTurret.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class UpgradeTurret : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Upgrade()
+    {
+        Debug.Log("Interakcja!!!");
+    }
+}
