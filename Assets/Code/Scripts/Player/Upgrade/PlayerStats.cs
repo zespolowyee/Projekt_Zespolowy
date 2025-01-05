1993abe0d45ea0805ccbfed532971e65ae4d9240
Justyna <justyna.komza@interia.pl>
2025-01-05 18:44:52 +0100

    dodanie skryptów dla ulepszeń gracza wypisujących postępy w konsoli
    na scenie gracz i trzy przyciski wywołujące ulepszenia health, damage, speed

---
+++ Assets/Code/Scripts/Player/Upgrade/PlayerStats.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour, IPlayerStats
+{
+    [Header("Player Stats")]
+    [SerializeField] private int health = 100;
+    [SerializeField] private int damage = 10;
+    [SerializeField] private float speed = 5f;
+
+    [Header("Experience Points")]
+    [SerializeField] private int currentEXP = 1000;
+
+    public void ApplyUpgrade(UpgradeEffects effects)
+    {
+        health += effects.healthBonus;
+        damage += effects.damageBonus;
+        speed += effects.speedBonus;
+
+        Debug.Log($"Upgrade applied! New stats: Health={health}, Damage={damage}, Speed={speed}");
+    }
+
+    public int GetCurrentEXP()
+    {
+        return currentEXP;
+    }
+
+    public void DeductEXP(int amount)
+    {
+        if (amount > currentEXP)
+        {
+            Debug.LogError("Not enough EXP!");
+            return;
+        }
+
+        currentEXP -= amount;
+        Debug.Log($"EXP deducted: {amount}. Remaining EXP: {currentEXP}");
+    }
+}
---
+++ Assets/Code/Scripts/Player/Upgrade/PlayerUpgradeTree.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerUpgradeTree", menuName = "Scriptable Objects/Player Upgrade Tree")]
+public class PlayerUpgradeTree : ScriptableObject
+{
+    [Tooltip("List of all upgrade nodes in the tree.")]
+    public List<UpgradeNode> nodes;
+}
+
+[Serializable]
+public class UpgradeNode
+{
+    [Header("Upgrade Info")]
+    [Tooltip("Unique ID for this node.")]
+    public string id;
+
+    [Tooltip("Description of the upgrade.")]
+    public string description;
+
+    [Tooltip("Cost in EXP to unlock this upgrade.")]
+    public int cost;
+
+    [Tooltip("Stat changes provided by this upgrade.")]
+    public UpgradeEffects effects;
+
+    [Tooltip("IDs of child nodes that can be unlocked after this.")]
+    public List<string> childNodes;
+
+    [Header("State")]
+    [Tooltip("Is this upgrade unlocked?")]
+    public bool isUnlocked;
+}
+
+[Serializable]
+public class UpgradeEffects
+{
+    [Tooltip("Additional health points.")]
+    public int healthBonus;
+
+    [Tooltip("Additional damage.")]
+    public int damageBonus;
+
+    [Tooltip("Additional speed.")]
+    public float speedBonus;
+}
