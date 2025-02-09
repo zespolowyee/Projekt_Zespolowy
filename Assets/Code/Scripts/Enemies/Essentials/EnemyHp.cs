30d4a8046572489ea9e57fbfea9dcb05ef83665b
czyzowsz <01178829@pw.edu.pl>
2025-02-09 20:09:12 +0100

    Added method to add exp for killing an enemy

--- Assets/Code/Scripts/Enemies/Essentials/EnemyHp.cs
+++ Assets/Code/Scripts/Enemies/Essentials/EnemyHp.cs
@@ -2,5 +2,26 @@ using UnityEngine;
 
 public class EnemyHp : HPSystem
 {
-	
+	public int expReward = 50;
+    private GameObject lastAttacker;
+    public void TakeDamageFromAttacker(int damage, GameObject attacker)
+    {
+        lastAttacker = attacker;
+        TakeDamage(damage);
+    }
+
+    protected override void Die()
+    {
+        base.Die();
+
+        if (lastAttacker != null)
+        {
+            PlayerStats playerStats = lastAttacker.GetComponent<PlayerStats>();
+            if (playerStats != null)
+            {
+                playerStats.AddEXP(expReward);
+                Debug.Log($"{lastAttacker.name} gained {expReward} XP for killing {gameObject.name}.");
+            }
+        }
+    }
 }
