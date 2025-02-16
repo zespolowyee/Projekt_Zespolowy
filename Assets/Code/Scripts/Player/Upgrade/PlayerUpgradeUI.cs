b6e3872580615327ebfe1ebfbcafd981b987ae80
Justyna <justyna.komza@interia.pl>
2025-02-16 16:57:26 +0100

    próba realizacji ulepszenia poprzez gui
    zmieniony kod dla ui i dodane elementy w scenie, ale nieskończone

--- Assets/Code/Scripts/Player/Upgrade/PlayerUpgradeUI.cs
+++ Assets/Code/Scripts/Player/Upgrade/PlayerUpgradeUI.cs
@@ -1,18 +1,65 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using TMPro;
 
 public class PlayerUpgradeUI : MonoBehaviour
 {
     [SerializeField] private PlayerUpgradeManager upgradeManager;
     [SerializeField] private PlayerUpgradeTree upgradeTree;
-    [SerializeField] private Text upgradeDetailsText;
+    [SerializeField] private TMP_Text upgradeDetailsText;
     [SerializeField] private Button upgradeButton;
+
+    //[SerializeField] private Transform buttonContainer;
+    //[SerializeField] private TMP_Text expText;
+
 
     private UpgradeNode selectedNode;
 
+
+    [SerializeField] private GameObject buttonPrefab;
+    [SerializeField] private Transform buttonParent; // Parent UI object to hold buttons
+
+
+    private void Start()
+    {
+        CreateUpgradeButtons();
+    }
+    public void CreateUpgradeButtons()
+    {
+        // Clear existing buttons before generating new ones
+        foreach (Transform child in buttonParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Create a button for each upgrade node
+        foreach (var node in upgradeTree.nodes)
+        {
+            // Instantiate a new button from a prefab
+            GameObject newButton = Instantiate(buttonPrefab, buttonParent);
+
+            // Set the text of the button to the node's description
+            newButton.GetComponentInChildren<Text>().text = node.description;
+
+            // Add listener for the button click to select this node
+            newButton.GetComponent<Button>().onClick.AddListener(() => SelectUpgradeNode(node));
+        }
+    }
+
+    public void SelectUpgradeNode(UpgradeNode node)
+    {
+        // Set the selected node
+        selectedNode = node;
+
+        // Optionally, display the upgrade details in the UI
+        DisplayUpgradeDetails(node);
+    }
     public void DisplayUpgradeDetails(UpgradeNode node)
     {
+        if (node == null) return;
+
+        selectedNode = node;
         string effectsText = "Effects:\n";
         foreach (var modifier in node.effects.modifiers)
         {
@@ -34,4 +81,6 @@ public class PlayerUpgradeUI : MonoBehaviour
             DisplayUpgradeDetails(selectedNode); // Refresh details
         }
     }
+
+
 }
