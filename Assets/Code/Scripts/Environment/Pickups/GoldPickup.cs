d4020639aa1c214a28fa58cbf597af81b0957fb1
WGrot <117313232+WGrot@users.noreply.github.com>
2025-02-23 11:56:29 +0100

    changed visuals for gold

--- Assets/Code/Scripts/Environment/Pickups/GoldPickup.cs
+++ Assets/Code/Scripts/Environment/Pickups/GoldPickup.cs
@@ -5,7 +5,16 @@ public class GoldPickup : NetworkBehaviour
 {
     [SerializeField] private int amount = 1;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private float animationForce;
 
+
+    public void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 direction = Random.insideUnitCircle.normalized;
+        direction += new Vector3(0, 1, 0) * animationForce;
+        rb.AddForce(direction, ForceMode.Impulse);
+    }
     public void SetAmount(int amount)
     {
         this.amount = amount;
