46e435345a6ea74c6a9ad2c0b015837828b57694
namlev <mikipatkowski@protonmail.com>
2025-03-09 13:17:02 +0100

    Add axeman with animations

---
+++ Assets/Code/Scripts/Player/PlayerClass/Classes/Axeman/AxemanClass.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AxemanClass : PlayerClass
+{
+    public AxemanClass()
+    {
+        className = "Axeman";
+        attackRange = 5.0f;
+        damage = 50;
+    }
+}
