fdeeedc0e637a6adbe3dd7508aac19723d5cef13
czyzowsz <01178829@pw.edu.pl>
2025-04-01 21:16:52 +0200

    Fixed attack synchronization

--- Assets/Code/Scripts/MagicAttack/Fireball.cs
+++ Assets/Code/Scripts/MagicAttack/Fireball.cs
@@ -1,61 +1,47 @@
 using Unity.Netcode;
 using UnityEngine;
-
-// Ten skrypt obsługuje zachowanie kuli ognia, w tym eksplozję i zadawanie obrażeń obszarowych.
+using System.Collections;
+[RequireComponent(typeof(Rigidbody))]
 public class Fireball : NetworkBehaviour
-{
-    public GameObject explosionEffect; // Prefab efektu eksplozji
-    private int damage;
-    private float explosionRadius;
-
-    public void SetDamage(int dmg)
-    {
-        damage = dmg;
-    }
-
-    public void SetExplosionRadius(float radius)
-    {
-        explosionRadius = radius;
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        ExplodeServerRpc();
-    }
-
-    [ServerRpc]
-    void ExplodeServerRpc()
-    {
-        Explode();
-    }
-
-    void Explode()
-    {
-        // Tworzenie efektu eksplozji
-        if (explosionEffect != null)
-        {
-            Instantiate(explosionEffect, transform.position, Quaternion.identity);
-        }
-        
-        // Wyszukiwanie obiektów w zasięgu eksplozji
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius);
-        foreach (Collider hit in hitColliders)
-        {
-            EnemyHp enemyHp = hit.GetComponent<EnemyHp>();
-            if (enemyHp != null)
-            {
-                enemyHp.TakeDamageFromSource(damage, gameObject);
-                Debug.Log($"Trafiono: {hit.gameObject.name}, zadano {damage} obrażeń");
-            }
-        }
-        
-        // Usunięcie kuli ognia po eksplozji
-        Destroy(gameObject);
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, explosionRadius);
-    }
+{ private int damage;
+     private float explosionRadius;
+ 
+     public void SetDamage(int dmg)
+     {
+         damage = dmg;
+     }
+ 
+     public void SetExplosionRadius(float radius)
+     {
+         explosionRadius = radius;
+     }
+ 
+     void OnCollisionEnter(Collision collision)
+     {
+         ExplodeServerRpc();
+     }
+ 
+     [ServerRpc]
+     void ExplodeServerRpc()
+     {
+         Explode();
+     }
+ 
+     void Explode()
+     {
+         // Wyszukiwanie obiektów w zasięgu eksplozji
+         Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius);
+         foreach (Collider hit in hitColliders)
+         {
+             EnemyHp enemyHp = hit.GetComponent<EnemyHp>();
+             if (enemyHp != null)
+             {
+                 enemyHp.TakeDamageFromSource(damage, gameObject);
+                 Debug.Log($"Trafiono: {hit.gameObject.name}, zadano {damage} obrażeń");
+             }
+         }
+         
+         // Usunięcie kuli ognia po eksplozji
+         Destroy(gameObject);
+     }
 }
--- Assets/Code/Scripts/MagicAttack/MagicAttack.cs
+++ Assets/Code/Scripts/MagicAttack/MagicAttack.cs
@@ -3,99 +3,97 @@ using System.Collections;
 using Unity.Netcode;
 using Unity.Multiplayer.Samples.Utilities.ClientAuthority;
 
-// Ten skrypt odpowiada za atakowanie przeciwników za pomocą kuli ognia.
-// Dodawaj go do prefabów będących postaciami gracza używającymi magii.
-// W Animation Controller musisz użyć animacji "CastSpell" z triggerem "Cast".
 public class MagicAttack : NetworkBehaviour
 {
     public GameObject fireballPrefab; // Prefab kuli ognia
-    public Transform firePoint; // Ustaw to w Inspectorze w Unity
-
-    public float fireballSpeed = 5f; // Prędkość kuli ognia
-    private int damage; // Obrażenia zadawane przez kulę ognia
-    private float attackRange;
-    
-    private ClientNetworkAnimator networkAnimator;
-    private bool isAttacking = false;
-    void Start()
-    {
-        if (!IsOwner)
-        {
-            enabled = false;
-        }
-        networkAnimator = GetComponent<ClientNetworkAnimator>();
-        PlayerClass playerClass = GetComponent<PlayerClass>();
-        if (playerClass != null)
-        {
-            damage = playerClass.damage;
-            attackRange = playerClass.attackRange;
-        }
-    }
-
-    void Update()
-    {
-        // Sprawdzenie, czy gracz nie jest martwy
-        HPSystem hpSystem = GetComponent<HPSystem>();
-        if (hpSystem != null && hpSystem.isDead)
-        {
-            return;
-        }
-        
-        // Wyzwolenie ataku na kliknięcie przycisku myszy (Fire1)
-        if (Input.GetButtonDown("Fire1") && !isAttacking)
-        {
-            Attack();
-        }
-    }
-    void Attack()
-    {
-        isAttacking = true;
-        networkAnimator.Animator.CrossFade("Attack", 0f);
-
-        // Rozpocznij korutynę, która poczeka na zakończenie animacji i wywoła CastFireballServerRpc
-        CastFireballServerRpc();
-        
-        // Start coroutine to reset isAttacking after the attack animation is finished
-        StartCoroutine(ResetAttack());
-    }
-
-    IEnumerator ResetAttack()
-    {
-        yield return new WaitForSeconds(0.4f);
-        yield return new WaitForSeconds(networkAnimator.Animator.GetCurrentAnimatorStateInfo(0).length);
-        isAttacking = false;
-    }
-
-    IEnumerator WaitForAnimationAndCastFireball(float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        Vector3 spawnPosition = firePoint.position; 
-        GameObject fireball = Instantiate(fireballPrefab, spawnPosition, firePoint.rotation);
-
-        NetworkObject networkObject = fireball.GetComponent<NetworkObject>();
-        if (networkObject != null)
-        {
-            networkObject.Spawn(); // Synchronizuj obiekt w sieci
-        }
-
-        Rigidbody rb = fireball.GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            rb.linearVelocity = transform.forward * fireballSpeed; // Kula ognia leci do przodu
-        }
-
-        Fireball fireballScript = fireball.GetComponent<Fireball>();
-        if (fireballScript != null)
-        {
-            fireballScript.SetDamage(damage);
-            fireballScript.SetExplosionRadius(attackRange);
-        }
-    }
-
-    [ServerRpc]
-    void CastFireballServerRpc()
-    {
-        StartCoroutine(WaitForAnimationAndCastFireball(1.0f));
-    }
+     public Transform firePoint; // Ustaw to w Inspectorze w Unity
+ 
+     public float fireballSpeed = 5f; // Prędkość kuli ognia
+     private int damage; // Obrażenia zadawane przez kulę ognia
+     private float attackRange;
+     
+     private ClientNetworkAnimator networkAnimator;
+     private bool isAttacking = false;
+     void Start()
+     {
+         if (!IsOwner)
+         {
+             enabled = false;
+         }
+         networkAnimator = GetComponent<ClientNetworkAnimator>();
+         PlayerClass playerClass = GetComponent<PlayerClass>();
+         if (playerClass != null)
+         {
+             damage = playerClass.damage;
+             attackRange = playerClass.attackRange;
+         }
+     }
+ 
+     void Update()
+     {
+         // Sprawdzenie, czy gracz nie jest martwy
+         HPSystem hpSystem = GetComponent<HPSystem>();
+         if (hpSystem != null && hpSystem.isDead)
+         {
+             return;
+         }
+         
+         // Wyzwolenie ataku na kliknięcie przycisku myszy (Fire1)
+         if (Input.GetButtonDown("Fire1") && !isAttacking)
+         {
+             Attack();
+         }
+     }
+     void Attack()
+     {
+         isAttacking = true;
+         networkAnimator.Animator.CrossFade("Attack", 0f);
+ 
+         // Rozpocznij korutynę, która poczeka na zakończenie animacji i wywoła CastFireballServerRpc
+         CastFireballServerRpc();
+         
+         // Start coroutine to reset isAttacking after the attack animation is finished
+         StartCoroutine(ResetAttack());
+     }
+ 
+     IEnumerator ResetAttack()
+     {
+         yield return new WaitForSeconds(0.4f);
+         yield return new WaitForSeconds(networkAnimator.Animator.GetCurrentAnimatorStateInfo(0).length);
+         isAttacking = false;
+     }
+ 
+     IEnumerator CastFireball()
+     {
+         yield return new WaitForSeconds(0.1f);
+         Vector3 spawnPosition = firePoint.position; 
+         GameObject fireball = Instantiate(fireballPrefab, spawnPosition, firePoint.rotation);
+ 
+         NetworkObject networkObject = fireball.GetComponent<NetworkObject>();
+         if (networkObject != null)
+         {
+             networkObject.Spawn(); // Synchronizuj obiekt w sieci
+         }
+ 
+         Rigidbody rb = fireball.GetComponent<Rigidbody>();
+         if (rb != null)
+         {
+             rb.linearVelocity = transform.forward * fireballSpeed; // Kula ognia leci do przodu
+         }
+ 
+         Fireball fireballScript = fireball.GetComponent<Fireball>();
+         if (fireballScript != null)
+         {
+             fireballScript.SetDamage(damage);
+             fireballScript.SetExplosionRadius(attackRange);
+         }
+         Destroy(fireball, 5.0f);
+     }
+ 
+     [ServerRpc]
+     void CastFireballServerRpc()
+     {
+         StartCoroutine(CastFireball());
+     }
 
 }
